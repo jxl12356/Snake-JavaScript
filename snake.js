7f48524279bbcd0015dd4c3f0bd5af7359f8c079
elf <88328859+jxl12356@users.noreply.github.com>
2025-05-29 21:57:43 +0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -15,7 +15,7 @@ const ground = new Image();
 ground.src = "img/ground.png";
 
 const foodImg = new Image();
-foodImg.src = "img/food.png";
+foodImg.src = "img/xlhfood.png";
 
 // load audio files
 
